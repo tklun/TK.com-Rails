abe6b73d6ecccea3a0508c3babd3605d4048c596
tom <tom@tomklun.com>
2011-05-23 16:46:07 -0500

    Initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TKComRails::Application.config.secret_token = 'ce7ad917748fb393930162a873e37a2093bb8b9a52bc2167b841dff889264cb6c842db8ebb1c783568f54356c0ff6758d29e11776e4ae95bc0c77debc394a512'
