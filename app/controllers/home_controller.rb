ca34eb0ad667cf8af7f138686d64ea0675fc433b
Tom Klun <tom@tomklun.com>
2011-05-26 16:30:35 -0500

    Commented again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @title = "Home"
+    # @top_posts = Post.find(:all, :limit => 5)
     @top_posts = Post.paginate( :all, :per_page => 5, :page => params[:page] ) 
+    
   end
 
 end
