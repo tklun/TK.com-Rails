38bcf3d46ce3ac5bcde4668a332d666d13f46197
tom <tom@tomklun.com>
2011-07-25 22:57:01 -0500

    Added last-fm content toggle

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-postScroll()
+	postScroll()
+	lastFM.toggle("#last-fm");
 })
 
 var postScroll = function() {
 	var maxScrollPos = 91;
-	if ($("body").hasClass("posts-long") || $("body").hasClass("work"))
+	var scrollClass = "scroll";
+	if ($("body").hasClass("posts-long") || $("body").hasClass("work")) {
 		$(window).scroll(function() {
 			var currentPos = $(window).scrollTop();
-			console.log(currentPos)
 			if (currentPos >= maxScrollPos) {
-				$("html").addClass("scroll");
+				$("html").addClass(scrollClass);
 			} else if (currentPos <= maxScrollPos) {
-				$("html").removeClass("scroll");
+				$("html").removeClass(scrollClass);
 			};
-		});	
-}
+		});
+	};
+};
+
+var lastFM = {
+	toggle : function(fmContent) {
+		$(".last-fm a, #last-fm").click(function(event) {
+			event.preventDefault();
+			$(fmContent).fadeToggle();
+		});
+	}
+};
