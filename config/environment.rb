a0ec18b3d96f7f0a1288c950734acc394e02cb8c
tom <tom@tomklun.com>
2011-07-27 20:25:31 -0500

    Add sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,11 @@ TKComRails::Application.initialize!
 # if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
 # Haml::Template.options[:ugly] = true
 # end
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
