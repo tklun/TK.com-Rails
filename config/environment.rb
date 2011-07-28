d87b7a6ad1f13398801971b7c20e014cc3b9a287
tom <tom@tomklun.com>
2011-07-28 00:18:13 -0500

    Caching test

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TKComRails::Application.initialize!
-# if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
-# Haml::Template.options[:ugly] = true
-# end
+if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
+  module Heroku
+    class StaticAssetsMiddleware
+      def cache_static_asset(reply)
+        return reply unless can_cache?(reply)
+
+        status, headers, response = reply
+
+        headers['Cache-Control'] = 'public, max-age=31556926' # 1 year
+
+        build_new_reply(status, headers, response)
+      end
+    end
+  endend
