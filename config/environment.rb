0990f27c566fb91870a541ad4ff44a408805cd26
tom <tom@tomklun.com>
2011-07-28 00:19:28 -0500

    Undo..

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TKComRails::Application.initialize!
-if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
-  module Heroku
-    class StaticAssetsMiddleware
-      def cache_static_asset(reply)
-        return reply unless can_cache?(reply)
-
-        status, headers, response = reply
-
-        headers['Cache-Control'] = 'public, max-age=31556926' # 1 year
-
-        build_new_reply(status, headers, response)
-      end
-    end
-  endend
+# if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
+# Haml::Template.options[:ugly] = true
+# end
