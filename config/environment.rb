848f971cebe64894f295c6a391540fede0dfa954
tom <tom@tomklun.com>
2011-07-28 00:46:03 -0500

    uglify

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TKComRails::Application.initialize!
-# if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
-# Haml::Template.options[:ugly] = true
-# end
+if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENV'] == 'triage'
+Haml::Template.options[:ugly] = true
+end
