d180c155ddb21bd8882c3df5e04a57c4bc22aed7
tom <tom@tomklun.com>
2011-07-27 21:56:57 -0500

    Added content header for gzip?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  rescue_from CanCan::AccessDenied do |exception|
-     flash[:error] = exception.message
-     redirect_to root_url
-   end   
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end   
+
+  def index
+    response.headers['Content-Encoding'] = 'gzip'
+    render :text => "Rendered at #{Time.now}"
+  end
 end
