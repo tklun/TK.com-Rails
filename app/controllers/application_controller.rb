be9c439a97ebf7431fa0b438ce4b0ee3dde57ed6
tom <tom@tomklun.com>
2011-07-28 23:00:10 -0500

    Rejammed assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  response.headers['Vary'] = 'Accept-Encoding'
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
