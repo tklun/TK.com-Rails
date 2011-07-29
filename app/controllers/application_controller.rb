0dec97943a2f52b6d5e1ef16e8d41481600b2cd3
tom <tom@tomklun.com>
2011-07-28 23:04:58 -0500

    custom headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  response.headers['Vary'] = 'Accept-Encoding'
+  before_filter :custom_headers
+  
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
   end   
+
+   private
+   def custom_headers
+     response.headers['Vary'] = 'Accept-Encoding'
+   end
 
 end
