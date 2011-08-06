58d34c92e882b04cf81b1e90f152c41f49788899
tom <tom@tomklun.com>
2011-08-06 10:08:14 -0500

    Fine tuned homepage styles.  Added limit to projects on the home page.  Updated About copy. Added more links on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   def index
     @title = "Home"
     @class = "home"
-    @projects = Project.all
-    
-    @tracks = Track.get_all_tracks
-    
+    @projects = Project.limit(2).order("created_at DESC")
+    @tracks = Track.get_all_tracks    
     # response.headers['Content-Encoding'] = 'gzip'
   end
 
