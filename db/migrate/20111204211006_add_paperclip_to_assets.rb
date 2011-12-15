405ed67314e0015d344c2624c80d51dc21390adb
Tom Klun <tom@tomklun.com>
2011-12-14 19:42:15 -0600

    Fixed migration

--- db/migrate/20111204211006_add_paperclip_to_assets.rb
+++ db/migrate/20111204211006_add_paperclip_to_assets.rb
@@ -1,8 +1,5 @@
 class AddPaperclipToAssets < ActiveRecord::Migration
   def self.up
-    remove_column :assets, :image_file_name
-    remove_column :assets, :image_content_type
-    remove_column :assets, :image_file_size
     add_column :assets, :photo_file_name, :string
     add_column :assets, :photo_content_type, :string
     add_column :assets, :photo_file_size, :integer
